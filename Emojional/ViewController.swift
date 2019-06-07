9d21914b1373887985ad4b93d69de39bd25b69a1
Apple <apple@Apples-MacBook-Pro-24.local>
2019-06-07 13:25:17 -0700

    Complete Iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -14,7 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func laughingEmoji(_ sender: UIButton) {
+        var alertController = UIAlertController(title: "That's awesome dude!", message: "Keep it up", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sadEmoji(_ sender: UIButton) {
+        var alertController = UIAlertController(title: "That sucks dude", message: "Feel better buddie", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
 }
 
